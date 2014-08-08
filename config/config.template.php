6a67d08d59dfe1517138a17d57b99bf704d6f1af
Atif Ghaffar <atif.ghaffar@gmail.com>
2014-08-08 13:51:25 +0200

    updating config.template

--- config/config.template.php
+++ config/config.template.php
@@ -1,13 +1,23 @@
 <?php
 /** Configuration Constants **/
 
-// Name of the project. The cache and compile directories will be created with this name
-define ('PROJECT_NAME','wbs_relmgr');
+// define ROOT. One level before this directory
+define('ROOT', dirname(__DIR__));
 
+// Name of the project. 
+define ('PROJECT_NAME','parsealbums');
 
-// This should set some caching to on or off
+// This should set some caching/debug, etc to on or off
 define ('DEVELOPMENT_ENVIRONMENT',true);
 
-// Define where the local repositories should be managed. This should be writable
-define ('LOCAL_REPOSITORY_DIR', __DIR__ . '/../repositories'); # Example /tmp/repositories
+
+// Smarty stuff
+define('SMARTY_COMPILE_DIR', 	ROOT . DS . 'tmp' .DS . 'smarty_compile');
+define('SMARTY_CACHE_DIR',		ROOT .DS . 'tmp' .DS . 'smarty_cache');
+
+
+// Parse stuff
+define('PARSE_APP_ID', 'YOUR-PARSE-APP-ID');
+define('PARSE_REST_KEY', 'YOUR-PARSE-REST-KEY');
+define('PARSE_MASTER_KEY', 'YOUR-PARSE-MASTER-KEY');
 
